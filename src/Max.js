fcf3ad3b46147c8cdf4d31308908736bb43d6602
Paul M. Christian <paulmchristian@gmail.com>
2018-01-28 21:21:52 -0600

    add connected indicator

--- src/Max.js
+++ src/Max.js
@@ -10,28 +10,43 @@ class Max extends Component {
     console.log('supported objs:', SUPPORTED_OBJECTS);
 
     this.xebraState = new State({
-      hostname: '127.0.0.1',
+      hostname: '192.168.1.37',
+      //hostname: '127.0.0.1',
       // hostname: '10.30.87.25',
       port: 8086,
       supported_objects: SUPPORTED_OBJECTS
     });
 
     this.xebraState.connect();
+
+    this.xebraState.on("connection_changed", status => {
+      //   INIT: 1,
+      //   CONNECTING: 2,
+      //   CONNECTED: 4,
+      //   CONNECTION_FAIL: 8,
+      //   RECONNECTING: 16,
+      //   DISCONNECTED: 32
+      this.props.onConnectionChange(CONNECTION_STATES.CONNECTED === status);
+    });
+
     // this.xebraState.getPatchers();
   }
 
-  render() {
-    const { floats } = this.props;
-
-    if (this.xebraState.connectionState === CONNECTION_STATES.CONNECTED) {
-      _.each(floats, (val, key) => {
-        this.xebraState.sendMessageToChannel(
-          'osc',
-          `${this.props.oscAddr}/${key} ${val}`
-        );
-      });
+  sendOscMsgs = (oscMsgs) => {
+    if (this.isConnected()) {
+      _.each(
+        oscMsgs,
+        oscMsg => this.xebraState.sendMessageToChannel(
+          'osc', oscMsg
+        ));
     }
+  };
 
+  isConnected = () => this.xebraState.connectionState === CONNECTION_STATES.CONNECTED;
+
+  render() {
+    const { oscMsgs } = this.props;
+    this.sendOscMsgs(oscMsgs);
     return null;
   }
 
